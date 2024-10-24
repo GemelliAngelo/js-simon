39864246bfda6d19a6f5f8e8339d753fa9a157a4
GemelliAngelo <gemeangelo27@gmail.com>
2024-10-24 16:02:15 +0200

    add numbers saves array

--- js/main.js
+++ js/main.js
@@ -1,30 +1,66 @@
+// # DATI
 const gameTimeEl = document.getElementById("time");
 const gameNumber1El = document.getElementById("number-1");
 const gameNumber2El = document.getElementById("number-2");
 const gameNumber3El = document.getElementById("number-3");
 const gameNumber4El = document.getElementById("number-4");
 const gameNumber5El = document.getElementById("number-5");
-let seconds = 30;
+const gameButtonEl = document.getElementById("game-button");
+let seconds = 5;
+
+// * FUNZIONE RANDOMICA
+function random(min, max) {
+  const randomNumber = Math.floor(Math.random() * max - min + 1) + 1;
+  return randomNumber;
+}
 
 // * GENERO I NUMERI E LI STAMPO IN PAGINA
-gameNumber1El.value = Math.floor(Math.random() * 99) + 1;
-gameNumber2El.value = Math.floor(Math.random() * 99) + 1;
-gameNumber3El.value = Math.floor(Math.random() * 99) + 1;
-gameNumber4El.value = Math.floor(Math.random() * 99) + 1;
-gameNumber5El.value = Math.floor(Math.random() * 99) + 1;
+gameNumber1El.value = random(1, 100);
+gameNumber2El.value = random(1, 100);
+gameNumber3El.value = random(1, 100);
+gameNumber4El.value = random(1, 100);
+gameNumber5El.value = random(1, 100);
+
+// * SALVO I NUMERI GENERATI
+const gameNumbers = [
+  gameNumber1El.value,
+  gameNumber2El.value,
+  gameNumber3El.value,
+  gameNumber4El.value,
+  gameNumber5El.value,
+];
+
+const userNumbers = [];
 
 // * GENERO FUNZIONE INPUT
-// const userInput = () => {};
+const userInput = () => {
+  // * ABILITO GLI INPUT
+  gameNumber1El.removeAttribute("disabled");
+  gameNumber2El.removeAttribute("disabled");
+  gameNumber3El.removeAttribute("disabled");
+  gameNumber4El.removeAttribute("disabled");
+  gameNumber5El.removeAttribute("disabled");
 
-// *GENERO IL COUNTDOWN
+  //  * SVUOTO GLI INPUT
+  gameNumber1El.value = "";
+  gameNumber2El.value = "";
+  gameNumber3El.value = "";
+  gameNumber4El.value = "";
+  gameNumber5El.value = "";
+};
+
+// * GENERO IL COUNTDOWN
 const countdown = () => {
   if (seconds <= 0) {
     gameTimeEl.innerText = "Tempo Scaduto!";
+    userInput();
     clearInterval(countdownInterval);
-    // userInput();
   } else gameTimeEl.innerText = seconds--;
 };
-countdown();
 
-// * SETTO L'INTERVALLO
+// * SETTO L'INTERVALLO E AVVIO IL COUNTDOWN
+("disabled");
+countdown();
 const countdownInterval = setInterval(countdown, 1000);
+
+// *
